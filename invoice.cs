f6bf3ee8bbcb4488f0227c236df1add79abf7880
Puck-The-Bard <zdmcguire1@buffs.wtamu.edu>
2019-11-05 14:49:23 -0600

    Co-authored-by: Ortiz0216 <djortiz3@buffs.wtamu.edu> Co-authored-by: Angelicaalonzo <aalonzo1@buffs.wtamu.edu>

---
+++ invoice.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+
+class Invoice
+{
+    int InvNum;
+    string nvDate;
+    float total;
+    
+    List<invoiceEntry> LineItems = new List<invoiceEntry>;
+
+    
+
+
+    public Invoice()
+    {
+        
+    }
+
+    public void addinvEntry()
+    {
+        
+    }
+
+    public void removeinvEntry()
+    {
+        
+    }
+    public void updateTotal(float t)
+    {
+        t = total;
+    }
+    public void updateLineNumbers()
+    {
+        
+    }
+
+    public void printInvoice()
+    {
+        
+    }
+
+
+}
+
